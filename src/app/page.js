cbdd73454a6e9bfeef88c8f9ba82ae80cc82e1b2
Benjamin Shuster <bentzishuster613@gmail.com>
2023-08-02 09:51:34 -0400

    add components to main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,23 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import TagLineName from '@/components/TagLineNameComponent/TagLineName'
+import Toolbox from '@/components/ToolboxComponent/Toolbox'
+import Projects from '@/components/ProjectsComponent/Projects'
+import About from '@/components/AboutComponent/About'
+import Subscribe from '@/components/SubscribeComponent/Subscribe'
+import Footer from '@/components/FooterComponent/Footer'
+import Blog from '@/components/BlogComponent/Blog'
 
 export default function Home() {
   return (
     <main>
-      
+      <TagLineName />
+      <Toolbox  />
+      <Projects />
+      <About />
+      <Blog />
+      <Subscribe />
+      <Footer />
     </main>
   )
 }
