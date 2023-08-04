1da6d44f2f64e1d5710da1302222a92bef6396b5
Benjamin Shuster <bentzishuster613@gmail.com>
2023-08-04 06:01:47 -0400

    add the border card component

--- src/components/AboutComponent/BorderCardComponent/BorderCard.jsx
+++ src/components/AboutComponent/BorderCardComponent/BorderCard.jsx
@@ -1,8 +1,8 @@
 import styles from "./bordercard.module.css"
 export default function BorderCard(props) {
 return (
-<div>
-<div>
+<div className={styles.bordercardborder}>
+<div className={styles.bordercard}>
 {props.children}
 </div>
 </div>
