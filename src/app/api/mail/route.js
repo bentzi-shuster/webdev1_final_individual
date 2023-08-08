7a0935084c4ee9bc93e29d7b6a17086eb43e58b0
Benjamin Shuster <bentzishuster613@gmail.com>
2023-08-08 08:48:21 -0400

    update email

--- src/app/api/mail/route.js
+++ src/app/api/mail/route.js
@@ -1,9 +1,16 @@
+import { NextResponse } from "next/server";
 import makeEmailToUser from "@/lib/sendMail";
-import { URL } from 'url';
 export async function GET(request){
+    try{
     //check if request is from my site
     let url = request.nextUrl.searchParams
     let email = url.get('email');
     let name = url.get('name');
     makeEmailToUser(email,name);
+    return NextResponse.json({message:"email sent"},{status:200});
+    }catch(e){
+        console.log(e);
+        return NextResponse.json({message:"email not sent"},{status:500});
+    }
+
 }
