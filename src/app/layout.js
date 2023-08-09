927c37481233892a80a514dfd962c7e759e76b50
Benjamin Shuster <bentzishuster613@gmail.com>
2023-08-09 07:28:03 -0400

    update GTM

--- src/app/layout.js
+++ src/app/layout.js
@@ -48,7 +48,20 @@ export default function RootLayout({ children }) {
   gtag('config', 'G-07J2SQ3D3C');
 
 `}} id="gtag"></Script>
+{/* <!-- Google Tag Manager (noscript) --> */}
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MHKW2KWS"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+{/* <!-- End Google Tag Manager (noscript) --> */}
 
+{/* <!-- Google Tag Manager --> */}
+<Script strategy='afterInteractive' dangerouslySetInnerHTML={{__html:`
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MHKW2KWS');
+`}} id="gtm"></Script>
+{/* <!-- End Google Tag Manager --> */}
       <Nav />
         {children}
 
